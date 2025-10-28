127e713c77573ba6d982386c321bd5282d74b5a5
cmcfarren23 <connor.mcfarren.23@cnu.edu>
2025-10-28 05:35:15 -0400

    Inital look of first question

---
+++ 01_Implementation/CMMCFledgeSystem/CMMCFledge_Assessment_Start.php
@@ -0,0 +1,54 @@
+<?php session_start(); ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CMMCFledge_style.css">
+    </head>
+
+    <body>
+        <div class = "homeHeader"> 
+            <div class = "homeHeaderLogo">
+                <a href="CMMCFledge_Home_Page.html">
+                    <img src="Images\CMMCFledge_Logo.png" alt="CMMC Fledge" style="width:128px;height:128px;">
+                </a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="CMMCFledge_Assessment_Start.php">Assessment</a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="CMMCFledge_Fledge_Dictionary.html">Fledge Dictionary</a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="CMMCFledge_About_Us.html">About Us</a>
+            </div>
+            <div class = "homeHeaderLogo"></div>
+        </div>
+
+        <div class="bodyColumnContainer">
+            <div class="bodyColumnWide">
+                <div class = "assessmentTitle">What Level of CMMC Certification is your Organization Pursuing?</div>
+                <div class = "assessmentSubTitle">(May be listed in current of future contracts(s))</div>
+                <div class = "questionInstruction"><br><br>Select One of The Following<br><br></div>
+                <div class = "questionRadioContainer">
+                    <form action="$_SESSION['user_name'] = $username;">
+                        <label> <input type="radio" name="CMMCCertType" value="l1">CMMC l1<br></label>
+                        <label> <input type="radio" name="CMMCCertType" value="l2Self">CMMC l2 (Self-Assessment)<br></label>
+                        <label> <input type="radio" name="CMMCCertType" value="l1C3PAO">CMMC l2 (C3PAO-Assessment)</label>
+                        <br>
+                        <div class = "singleSubmit">
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+            
+        <div class = "homeFooter"> <b>NOTICE:</b> This is a student designed system. All information found within the CMMC Fledge System or related systems are for informational purposes only. 
+            This is a student project and shall not be used as a substitute for professional advice. Use this system at your own risk.
+        </div>
+
+
+    </body>
+</html>
