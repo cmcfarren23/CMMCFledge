ae737e73e34ebed9211cdfc10349df0dda8ac09d
cmcfarren23 <connor.mcfarren.23@cnu.edu>
2025-11-01 21:39:33 -0400

    Gitgnore 2.0

--- 01_Implementation/CMMCFledgeSystem/Include/DBConnect.php
+++ 01_Implementation/CMMCFledgeSystem/Include/DBConnect.php
@@ -6,6 +6,7 @@ $password   = "";
 $dbname     = "CMMCFledge";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+  
 
 if($conn->connect_error){
         die("connection failed: ". $conn->connect_error);
