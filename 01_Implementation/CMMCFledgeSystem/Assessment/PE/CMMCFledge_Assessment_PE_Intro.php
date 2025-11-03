66f7737638e3e20e268aecb136191b0585d2cea5
cmcfarren23 <connor.mcfarren.23@cnu.edu>
2025-11-02 23:25:17 -0500

    Added intros

---
+++ 01_Implementation/CMMCFledgeSystem/Assessment/PE/CMMCFledge_Assessment_PE_Intro.php
@@ -0,0 +1,51 @@
+<?php 
+    session_start(); 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../../Include/CMMCFledge_Style.css">
+    </head>
+
+    <body>
+        <div class = "homeHeader"> 
+            <div class = "homeHeaderLogo">
+                <a href="..\..\CMMCFledge_Home_Page.html">
+                    <img src="..\..\Images\CMMCFledge_Logo.png" alt="CMMC Fledge" style="width:128px;height:128px;">
+                </a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="..\CMMCFledge_Assessment_Start.php">Assessment</a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="..\..\CMMCFledge_Fledge_Dictionary.html">Fledge Dictionary</a>
+            </div>
+            <div class = "homeHeaderDiv">
+                <a href ="..\..\CMMCFledge_About_Us.html">About Us</a>
+            </div>
+            <div class = "homeHeaderLogo"></div>
+        </div>
+
+        <div class="bodyColumnContainer">
+            <div class="bodyColumnWide">
+                <div class = "assessmentTitle">This section will cover general questions about Physical Protection (PE)</div>
+                <!-- <div class = "assessmentSubTitle">Select One of the Following</div> -->
+                <div class = "questionInstruction"><br><br>Answer the following questions to the best of your ability</div>
+                <div class = "questionRadioContainer">
+                    <form method="post" >
+                        <div class = "singleSubmit">
+                            <a href ="CMMCFledge_Assessment_CM_Baseline.php"><button type="button">Continue</button></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+            
+        <div class = "homeFooter"> <b>NOTICE:</b> This is a student designed system. All information found within the CMMC Fledge System or related systems are for informational purposes only. 
+            This is a student project and shall not be used as a substitute for professional advice. Use this system at your own risk.
+        </div>
+
+
+    </body>
+</html>
